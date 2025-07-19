7da1ac4b15938977fffe85108c3dfc1174d67d63
Shunazo <abeljunior0907@gmail.com>
2025-07-19 01:14:16 -0400

    Added Usuario and Administrador routes, / Installed additional packages

--- backend/routes/administrador.js
+++ backend/routes/administrador.js
@@ -4,10 +4,21 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const auth = require("../middleware/is-auth");
 
-//router.get("/home", auth, adminController.home);
-//TODO: 
-// Ruta para ver todos los usuarios (get) 
-// Ruta para crear/editar administrador (get/post)
-//Ruta para activar/desactivar usuarios (post)
+router.get("/home", auth, adminController.home);
+
+router.get("/perfil/edit", auth, adminController.editPerfilForm);
+router.post("/perfil/edit", auth, adminController.editPerfil);
+
+router.get("/usuarios", auth, adminController.usuarios);
+router.post("/usuarios/activate/:id", auth, adminController.activateUser);
+router.post("/usuarios/deactivate/:id", auth, adminController.deactivateUser);
+
+router.get("/administradores", auth, adminController.administradores);
+router.get("/administradores/create", auth, adminController.createAdminForm);
+router.post("/administradores/create", auth, adminController.createAdmin);
+router.get("/administradores/edit/:id", auth, adminController.editAdminForm);
+router.post("/administradores/edit/:id", auth, adminController.editAdmin);
+router.post("/administradores/activate/:id", auth, adminController.activateAdmin);
+router.post("/administradores/deactivate/:id", auth, adminController.deactivateAdmin);
 
 module.exports = router;
--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -4,15 +4,25 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const auth = require("../middleware/is-auth");
 
-//router.get("/home", auth, usuarioController.home);
+router.get("/home", auth, usuarioController.home);
 
+router.get("/perfil/edit", auth, usuarioController.editPerfilForm);
+router.post("/perfil/edit", auth, usuarioController.editPerfil);
 
-//TODO: 
-// Ruta para editar perfil (get/post)
-// Ruta para ver categorias (get)
-// Ruta para ver gastos (get)
-//Ruta para crear/editar/borrar categorias (get/post)
-//Ruta para crear/editar/borrar gastos (get/post)
-//Ruta para agregar ingresos (get/post)
+router.get("/categorias", auth, usuarioController.categorias);
+router.get("/gastos", auth, usuarioController.gastos);
+
+router.get("/categorias/create", auth, usuarioController.createCategoriaForm);
+router.post("/categorias/create", auth, usuarioController.createCategoria);
+router.get("/categorias/edit/:id([0-9]+)", auth, usuarioController.editCategoriaForm);
+router.post("/categorias/edit/:id([0-9]+)", auth, usuarioController.editCategoria);
+router.post("/categorias/delete/:id([0-9]+)", auth, usuarioController.deleteCategoria);
+
+router.get("/gastos/create", auth, usuarioController.createGastoForm);
+router.post("/gastos/create", auth, usuarioController.createGasto);
+router.get("/gastos/edit/:id([0-9]+)", auth, usuarioController.editGastoForm);
+router.post("/gastos/edit/:id([0-9]+)", auth, usuarioController.editGasto);
+router.post("/gastos/delete/:id([0-9]+)", auth, usuarioController.deleteGasto);
+
 
 module.exports = router;
